c2f6ac59c0db50b60c2da9db06f8b4d29c03d05e
tiago <Tiago>
2015-01-19 21:10:50 +0100

    mocked version

--- movies/movies_sl3.rb
+++ movies/movies_sl3.rb
@@ -1,21 +1,55 @@
-class SL3
-  def result_count
+#encoding: utf-8
+require 'imdb'
 
+class Movie
+  attr_reader :title, :rating, :seasons, :episodes
+  def initialize title, rating, seasons, episodes
+    @title = title
+    @seasons = seasons
+    @episodes = episodes
+    @rating = rating
+  end
+end
+
+class SL3
+  def initialize
+    @movies = [
+      Movie.new("Game of Thrones", 9.5, 6, 60),
+      Movie.new("American Horror Story", 8.4, 5, 52),
+      Movie.new("The Walking Dead", 8.7, 6, 69),
+      Movie.new("Sons of Anarchy", 8.7, 8, 95),
+      Movie.new("Breaking Bad", 9.6, 5,62)
+    ]
+  end
+
+  def result_count(search)
+    # Imdb::Search.new(search).movies.size
+    200
   end
 
   def most_seasons_from
-  	
+    @movies.sort {|a,b| a.seasons <=> b.seasons}.last.title
   end
 
   def most_episodes_from
-
-  end
-
-  def most_episodes_from
-
+    @movies.sort {|a,b| a.episodes <=> b.episodes}.last.title
   end
 
   def best_rated
+    @movies.sort {|a,b| a.rating <=> b.rating}.last.title
+  end
 
+  def top_list(top_count)
+  	b_rated = @movies.sort {|a,b| a.rating <=> b.rating}
+  	b_rated.slice(-top_count,top_count).map{|item| item.title}
+  end
+
+  private
+  def getMovie(name)
+    movie = Imdb::Search.new(search).movies.first
+    dexter_serie = Imdb::Serie.new(movie.id)
   end
 end
+
+sl3 = SL3.new
+puts sl3.result_count("Cat")
--- movies/movies_test_sl3.rb
+++ movies/movies_test_sl3.rb
@@ -1,6 +1,5 @@
 #encoding: utf-8
-require_relative 'd103_tvshows.rb'
-
+require_relative 'movies_sl3.rb'
 
 describe SL3 do
   before do
@@ -10,39 +9,42 @@ describe SL3 do
   describe "#result_count" do
     it "Should return number of results for a given string" do
       result_count = @series.result_count("Cat")
-      expect(result_count).to be > (200)
+      expect(result_count).to be >= (200)
     end
   end
 
   describe "#most_seasons_from" do
     it "Should return the serie with most seasons" do
-      most_seasons_from = @series.most_seasons_from([
-      "Breaking Bad","Friends","Game of Thrones","The Office"])
-      expect(most_seasons_from).to eq("Friends")
+      most_seasons_from = @series.most_seasons_from()
+      # most_seasons_from = @series.most_seasons_from([
+      # "Breaking Bad","Friends","Game of Thrones","The Office"])
+      expect(most_seasons_from).to eq("Sons of Anarchy")
     end
   end
 
   describe "#most_episodes_from" do
     it "Should return the serie with most episodes" do
-      most_episodes_from = @series.most_episodes_from([
-      "Breaking Bad","Friends","Game of Thrones","The Office"])
-      expect(most_episodes_from).to eq("Friends")
+      most_episodes_from = @series.most_episodes_from()
+      # most_episodes_from = @series.most_episodes_from([
+      # "Breaking Bad","Friends","Game of Thrones","The Office"])
+      expect(most_episodes_from).to eq("Sons of Anarchy")
     end
   end
 
   describe "#best_rated" do
     it "Should return the best rated serie" do
-      best_rated = @series.best_rated([
-      "Breaking Bad", "Pacific Blue", "The Affair"])
+      # best_rated = @series.best_rated([
+      # "Breaking Bad", "Pacific Blue", "The Affair"])
+      best_rated = @series.best_rated()
       expect(best_rated).to eq("Breaking Bad")
     end
   end
 
-  describe "#top_rated" do
+  describe "#top_list" do
     it "Should return the n best rated series" do
-      top_rated = @series.top_rated(3)
-      expect(top_rated).to eq(
-      	["Breaking Bad", "Pacific Blue", "The Affair"])
+      top_list = @series.top_list(3)
+      expect(top_list).to eq(
+      	["Sons of Anarchy", "Game of Thrones", "Breaking Bad"])
     end
   end
 
